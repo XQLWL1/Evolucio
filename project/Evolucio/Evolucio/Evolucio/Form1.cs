c7f8d1e8721ccc05c5f74ab65339afd6c44276e6
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-12-28 10:57:11 +0100

    New generation

--- project/Evolucio/Evolucio/Evolucio/Form1.cs
+++ project/Evolucio/Evolucio/Evolucio/Form1.cs
@@ -68,6 +68,45 @@ namespace Evolucio
 
             //hogyan lehet a legjobbakat megkapni?
             var topplayers = playerList.Take(populationSize / 2).ToList();
+
+            /*az 50 legjobb játékos megy a kövi körbe
+             * az új körben is 100-an indulnak, akik közül 50-en úgy viselkedik, mint az előző körben lévő legjobb 50*/
+            gc.ResetCurrentLevel();
+
+            foreach (Player p in topplayers)
+            {
+                //játékosok agyának klónozása
+                Brain b = p.Brain.Clone();
+
+
+                /*Ha a generáció 3-al osztva 0 maradékot ad, akkor meghívja a ExpandBrain (létrehoz egy új játékost és kiterjeszti az agyát) függvényt
+                 * ha hamis, akkor csak hozzáadjuk az agy alapjá*/
+                if (generation % 3 == 0)
+                {
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                }
+
+                else
+                {
+                    gc.AddPlayer(b);
+                }
+
+                /*Ha a generáció 3-al osztva 0 maradékot ad, akkor meghívja a ExpandBrain (létrehoz egy új játékost és kiterjeszti az agyát) függvényt
+                 * ha nem osztható 3-mal, akkor nem lesz kiterjesztve az agya */
+
+                if (generation % 3 == 0)
+                {
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                }
+                else
+                {
+                    gc.AddPlayer(b.Mutate());
+                }
+                    
+            }
+
+            //újabb kör indítása
+            gc.Start();
         }
 
     }
