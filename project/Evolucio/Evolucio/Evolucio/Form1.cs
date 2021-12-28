7dea91b200053c4e559f4306a25a246cae99e1d3
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-12-28 09:53:10 +0100

    gameplace

--- project/Evolucio/Evolucio/Evolucio/Form1.cs
+++ project/Evolucio/Evolucio/Evolucio/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Evolucio
 {
     public partial class Form1 : Form
     {
+
+        //referenciához hozzá lett adva a dll, ezért itt fogunk tudni csinálni egy GameController osztályt a form 1 szintjén
+
+        GameController gc = new GameController();
+        GameArea ga = new GameArea();
+
+
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
